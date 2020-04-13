cc56948a370c3a84f6d6f576c7952821d8c1d684
rjwilson0531 <rjwilson0531@gmail.com>
2020-04-13 11:51:18 -0400

    format

--- src/App.js
+++ src/App.js
@@ -1,55 +1,70 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom' // switch
-import RestaurantContainer from './containers/RestaurantContainer';
-import FoodContainer from './containers/FoodContainer';
-import Navbar from './components/Navbar'
+import React from "react";
+import { Route, Switch } from "react-router-dom"; // switch
+import RestaurantContainer from "./containers/RestaurantContainer";
+import FoodContainer from "./containers/FoodContainer";
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       restaurants: [],
-      cart: []
-    }
+      cart: [],
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("http://localhost:3000/restaurants")
-    .then(resp => resp.json())
-    .then(json => {this.setState({restaurants: json})})
+      .then((resp) => resp.json())
+      .then((json) => {
+        this.setState({ restaurants: json });
+      });
   }
 
   addItemToCart = (event) => {
-    event.preventDefault()
-    console.log("clicked add to cart button")
-  }
+    event.preventDefault();
+    console.log("clicked add to cart button");
+  };
 
-  render(){
+  render() {
     return (
-    <div className="App">
-      <Navbar cart="I am a prop for navbar" />
+      <div className="App">
+        <Navbar cart="I am a prop for navbar" />
 
-      <Switch>
+        <Switch>
+          <Route
+            exact
+            path="/restaurant/:id"
+            render={(props) => {
+              console.log(props);
+              let restaurantId = parseInt(props.match.params.id);
+              if (this.state.restaurants.length > 0) {
+                let foundrestaurant = this.state.restaurants.find(
+                  (r) => r.id === restaurantId
+                );
+                console.log(foundrestaurant);
+                return (
+                  <FoodContainer
+                    restaurant={foundrestaurant}
+                    addItem={this.addItemToCart}
+                  />
+                );
+              } else {
+                return null;
+              }
+            }}
+          />
 
-        <Route exact path="/restaurant/:id" render={(props) =>{
-            console.log(props)
-            let restaurantId = parseInt(props.match.params.id)
-            if (this.state.restaurants.length > 0) {
-              let foundrestaurant = this.state.restaurants.find(r => r.id === restaurantId)
-              console.log(foundrestaurant)
-            return <FoodContainer restaurant={foundrestaurant} addItem = {this.addItemToCart} />
-            }else {
-              return null
-            }
-        }}/>
-
-        <Route exact path="/" render={() => <RestaurantContainer restaurants={this.state.restaurants} />}/>
-
-      </Switch>
-
-      
-    </div>
-  )
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <RestaurantContainer restaurants={this.state.restaurants} />
+            )}
+          />
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const RestaurantCard = (props) => {
-    return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.restaurant.img_url} />
-            <Card.Body>
-            <Card.Title>{props.restaurant.name}</Card.Title>
-            <Card.Text>
-                <div>Location: {props.restaurant.location}</div>
-                <div>Price point: {props.restaurant.price_point}</div>   
-            </Card.Text>
-            <Button variant="primary" href={`http://localhost:3001/restaurant/${props.restaurant.id}`} >Order</Button>
-            </Card.Body>
-            </Card>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={props.restaurant.img_url} />
+        <Card.Body>
+          <Card.Title>{props.restaurant.name}</Card.Title>
+          <Card.Text>
+            <div>Location: {props.restaurant.location}</div>
+            <div>Price point: {props.restaurant.price_point}</div>
+          </Card.Text>
+          <Button
+            variant="primary"
+            href={`http://localhost:3001/restaurant/${props.restaurant.id}`}
+          >
+            Order
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-export default RestaurantCard
+export default RestaurantCard;
